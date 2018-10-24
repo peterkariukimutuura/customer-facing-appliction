7d573c38f9b646ac33dafccccc4d0bdde8a9026b
peterkariukimutuura <kariukipeter30@gmail.com>
2018-10-24 19:56:23 +0300

    research and test repo for customer facing application development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule , Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { CrisislistComponent } from './crisislist/crisislist.component';
+import { HerolistComponent } from './herolist/herolist.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+
 import { MessagesComponent } from './messages/messages.component';
+
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisislistComponent },
+  { path: 'hero/:id', component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HerolistComponent,
+    data: { title: 'Heroes List' }
+  },
+  {
+    path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    CrisislistComponent,
+    HerolistComponent,
+    PagenotfoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
     BrowserModule,
     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
